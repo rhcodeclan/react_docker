ee6451dec8b58b4b48420af22dce2574a32d32f6
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-11-04 17:57:39 +0100

    removed profile from compose file

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This has been tested and published automatically through jenkins!
         </p>
         <a
           className="App-link"
